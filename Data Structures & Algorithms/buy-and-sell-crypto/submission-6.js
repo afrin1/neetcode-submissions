1e81257536788bf296b854e8e09519757e1232fe
afrin1 <afrinraeshu@gmail.com>
2026-08-01 12:48:29 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let left = 0;
+        let right = left+1;
+        let maxProfit = 0;
+
+        if (prices.length <= 0) {
+            return maxProfit;
+        }
+
+        while (right < prices.length) { 
+          if (prices[left] <= prices[right]) {
+            const profit = prices[right]-prices[left];
+            maxProfit = Math.max(profit, maxProfit);
+          }  
+          else {
+            left= right;
+          }
+          right++;
+        }
+        return maxProfit;
+    }
+}
